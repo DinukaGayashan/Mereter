4cd823cc47dbdd4f9b36eedb72287d40c2b5992e
Dinuka Gayashan <dinukagayashankasthuriarachchi@gmail.com>
2022-03-08 17:58:09 +0530

    add web app

---
+++ Web Application/js/opd.js
@@ -0,0 +1,38 @@
+
+
+function writePrescriptions() {
+  var x = document.getElementById("writePrescriptions");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function viewPrescriptions() {
+  var x = document.getElementById("viewPrescriptions");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function writePrescriptions2() {
+  var x = document.getElementById("writePrescriptions2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function viewPrescriptions2() {
+  var x = document.getElementById("viewPrescriptions2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
